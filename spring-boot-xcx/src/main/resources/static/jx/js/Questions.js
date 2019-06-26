9565d9680051fd265708b47f4ab0f3f7887f167f
zhangqianjin <zhangqianjin@163.com>
2019-06-26 17:33:13 +0800

    20190626下午

--- spring-boot-xcx/src/main/resources/static/jx/js/Questions.js
+++ spring-boot-xcx/src/main/resources/static/jx/js/Questions.js
@@ -123,266 +123,341 @@ var QuestionJosn = [
 			"questionAnswer" : "c"
 		} ];
 
-var HH = 0;//时
-var mm = 0;//分
-var ss = 0;//秒
-var timeState = true;//时间状态 默认为true 开启时间
-var questions= QuestionJosn;
-var itemList=["A","B","C","D","E","F"]
-var activeQuestion=0; //当前操作的考题编号
+var HH = 0;// 时
+var mm = 0;// 分
+var ss = 0;// 秒
+var timeState = true;// 时间状态 默认为true 开启时间
+var questions = QuestionJosn;
+var itemList = [ "A", "B", "C", "D", "E", "F" ]
+var activeQuestion = 0; // 当前操作的考题编号
 var questioned = 0; //
-var checkQues = []; //已做答的题的集合
-/*实现计时器*/
-var time = setInterval(function () {
-    if (timeState) {
-        if (HH == 24) HH = 0;
-        str = "";
-        if (++ss == 60) {
-            if (++mm == 60) { HH++; mm = 0; }
-            ss = 0;
-        }
-        str += HH < 10 ? "0" + HH : HH;
-        str += ":";
-        str += mm < 10 ? "0" + mm : mm;
-        str += ":";
-        str += ss < 10 ? "0" + ss : ss;
-        $(".time").text(str);
-    } else {
-        $(".time").text(str);
-    }
+var checkQues = []; // 已做答的题的集合
+/* 实现计时器 */
+
+var time = setInterval(function() {
+	if (timeState) {
+		if (HH == 24)
+			HH = 0;
+		str = "";
+		if (++ss == 60) {
+			if (++mm == 60) {
+				HH++;
+				mm = 0;
+			}
+			ss = 0;
+		}
+		str += HH < 10 ? "0" + HH : HH;
+		str += ":";
+		str += mm < 10 ? "0" + mm : mm;
+		str += ":";
+		str += ss < 10 ? "0" + ss : ss;
+		$(".time").text(str);
+	} else {
+		$(".time").text(str);
+	}
 }, 1000);
-//展示考卷信息
-function showQuestion(id){
-    $(".questioned").text(id+1);
-    questioned = (id+1)/questions.length
-    if(activeQuestion!=undefined){
-        $("#ques"+activeQuestion).removeClass("question_id").addClass("active_question_id");
-    }
-    activeQuestion = id;
-    $(".question").find(".question_info").remove();
-    $(".question").find(".quest-sum").remove();
-    var question = questions[id];
-    $(".question_title").html("<strong>第 "+(id+1)+" 题 、</strong>"+question.questionTitle);
-    var items = question.questionItems.split(";");
-    var item="";
-    $(".question").append('<li class="quest-sum"><img class="question-img" alt="" src="../../static/jx/img/c.png"></li>')
-    for(var i=0;i<items.length;i++){
-        item ="<li class='question_info' onclick='clickTrim(this)' id='item"
-                +i+"'><input type='radio' name='item' value='"+itemList[i]+"'>&nbsp;"+itemList[i]+"."+items[i]+"</li>";
-        $(".question").append(item);
-    }
-    $(".question").attr("id","question"+id);
-    $("#ques"+id).removeClass("active_question_id").addClass("question_id");
-    for(var i=0;i<checkQues.length;i++){
-        if(checkQues[i].id==id){
-            $("#"+checkQues[i].item).find("input").prop("checked","checked");
-            $("#"+checkQues[i].item).addClass("clickTrim");
-            $("#ques"+activeQuestion).removeClass("question_id").addClass("clickQue");
-        }
-    }
-    
+// 展示考卷信息
+function showQuestion(id) {
+	$(".questioned").text(id + 1);
+	questioned = (id + 1) / questions.length
+	if (activeQuestion != undefined) {
+		$("#ques" + activeQuestion).removeClass("question_id").addClass(
+				"active_question_id");
+	}
+	activeQuestion = id;
+	$(".question").find(".question_info").remove();
+	$(".question").find(".quest-sum").remove();
+	var question = questions[id];
+	var htmlpj = '<a onclick="musicplay()" href="javascript:;" class=" weui-btn_default-zdy"><img class="jq-left-img"  src="../../static/jx/img/sy/dt.png"></a>'
+	$(".question_title").html(
+			"<strong>第 " + (id + 1) + " 题 、</strong>" + question.questionTitle
+					+ htmlpj);
+	$('#showjq').html('是因为 span 不是块状元素。本身自带有 左浮动的效果');
+	$('.bzda-db').html(
+			'是因为 span 不是块状元素。本身自带有 左浮动的效果，并且连续的英文字母跟数字是没办法 自动换行的 必须要 强制换行。')
+	var items = question.questionItems.split(";");
+	var item = "";
+	$(".question")
+			.append(
+					'<li class="quest-sum"><img class="question-img" alt="" src="../../static/jx/img/c.png"></li>')
+	for (var i = 0; i < items.length; i++) {
+		item = "<li class='question_info' onclick='clickTrim(this)' id='item"
+				+ i + "'><input type='radio' name='item' value='" + itemList[i]
+				+ "'>&nbsp;" + itemList[i] + "." + items[i] + "</li>";
+		$(".question").append(item);
+	}
+	$(".question").attr("id", "question" + id);
+	$("#ques" + id).removeClass("active_question_id").addClass("question_id");
+	for (var i = 0; i < checkQues.length; i++) {
+		if (checkQues[i].id == id) {
+			$("#" + checkQues[i].item).find("input").prop("checked", "checked");
+			$("#" + checkQues[i].item).addClass("clickTrim");
+			$("#ques" + activeQuestion).removeClass("question_id").addClass(
+					"clickQue");
+		}
+	}
+
 }
 
-/*答题卡*/
-function answerCard(){
-    $(".question_sum").text(questions.length);
-    for(var i=0;i<questions.length;i++){
-        var questionId ="<li id='ques"+i+"'onclick='saveQuestionState("+i+")' class='questionId'>"+(i+1)+"</li>";
-        $("#answerCard ul").append(questionId);
-    }
+/* 答题卡 */
+function answerCard() {
+	$(".question_sum").text(questions.length);
+	for (var i = 0; i < questions.length; i++) {
+		var questionId = "<li id='ques" + i + "'onclick='saveQuestionState("
+				+ i + ")' class='questionId'>" + (i + 1) + "</li>";
+		$("#answerCard ul").append(questionId);
+	}
 }
-/*展现错误的答题卡*/
-function answerCardcw(){
-    debugger;
-    $('.list-unstyled-zdy').empty();
-    for(var i=0;i<checkQues.length;i++){
-    	if(checkQues[i].zqda==0){
-    		 var questionId ="<li id='ques"+i+"'onclick='saveQuestionState("+i+")' class='questionIdcw'>"+(i+1)+"</li>";
-    	        $("#answerCardcw1 ul").append(questionId);
-    	}
-       
-    }
+/* 展现错误的答题卡 */
+function answerCardcw() {
+	debugger;
+	$('.list-unstyled-zdy').empty();
+	for (var i = 0; i < checkQues.length; i++) {
+		if (checkQues[i].zqda == 0) {
+			var questionId = "<li id='ques" + checkQues[i].id
+					+ "'onclick='saveQuestionState(" + checkQues[i].id
+					+ ")' class='questionIdcw'>" + eval((checkQues[i].id)+1) + "</li>";
+			$("#answerCardcw1 ul").append(questionId);
+		}
+
+	}
 }
-/*选中考题*/
+/* 选中考题 */
 var Question;
-function clickTrim(source){
-    var id = source.id;
-    $("#"+id).find("input").prop("checked","checked");
-    $("#ques"+activeQuestion).removeClass("question_id").addClass("clickQue");
-    var ques =0;
-    var zqid;
-    //正确答案
-   var zqda= questions[activeQuestion].questionAnswer;
-   if(zqda==$("#"+id).find("input[name=item]:checked").val()){
-	   $("#"+id).addClass("clickTrim");
-	   for(var i=0;i<checkQues.length;i++){
-	       if( checkQues[i].id==activeQuestion&&checkQues[i].item!=id){
-	           ques = checkQues[i].id;
-	           checkQues[i].item = id;//获取当前考题的选项ID
-	           checkQues[i].answer =$("#"+id).find("input[name=item]:checked").val();//获取当前考题的选项值
-	           checkQues[i].zqda=1;
-	       }
-	    }
-	    
-	    if(checkQues.length==0||Question!=activeQuestion&&activeQuestion!=ques){
-	        var check ={};
-	        check.id=activeQuestion;//获取当前考题的编号
-	        check.item = id;//获取当前考题的选项ID
-	        check.answer =$("#"+id).find("input[name=item]:checked").val();//获取当前考题的选项值
-	        check.zqda=1;
-	        checkQues.push(check);
-	    }
-	   if((activeQuestion+1)!=questions.length){
-	    	showQuestion(activeQuestion+1);
-	    } else{
-	    	 showQuestion(activeQuestion)
-	    }
-   }else{
-	   $("#"+id).addClass("clickTrimcw");
-	   for(var i=0;i<checkQues.length;i++){
-	       if( checkQues[i].id==activeQuestion&&checkQues[i].item!=id){
-	           ques = checkQues[i].id;
-	           checkQues[i].item = id;//获取当前考题的选项ID
-	           checkQues[i].answer =$("#"+id).find("input[name=item]:checked").val();//获取当前考题的选项值
-	           checkQues[i].zqda=0;
-	       }
-	    }
-	    
-	    if(checkQues.length==0||Question!=activeQuestion&&activeQuestion!=ques){
-	        var check ={};
-	        check.id=activeQuestion;//获取当前考题的编号
-	        check.item = id;//获取当前考题的选项ID
-	        check.answer =$("#"+id).find("input[name=item]:checked").val();//获取当前考题的选项值
-	        check.zqda=0;
-	        checkQues.push(check);
-	    }
-	    //把正确的答案显示出来
-	    $(".question_info").each(function(){
-	        var otherId =$(this).attr("id");
-	        if(zqda==$("#"+otherId).find("input[name=item]").val()){
-	        	zqid=otherId;
-	        	$("#"+otherId).addClass("clickTrim");
-	        }
-	    })
-   }
-   
-    $(".question_info").each(function(){
-        var otherId =$(this).attr("id");
-        if(otherId!=id && otherId!=zqid){
-            $("#"+otherId).find("input").prop("checked",false);
-            $("#"+otherId).removeClass("clickTrim");
-            $("#"+otherId).removeClass("clickTrimcw");
-        }
-    })
-    Question = activeQuestion;
-    showctlv();
-    
+function clickTrim(source) {
+	var id = source.id;
+	$("#" + id).find("input").prop("checked", "checked");
+	$("#ques" + activeQuestion).removeClass("question_id").addClass("clickQue");
+	var ques = 0;
+	var zqid;
+	// 正确答案
+	var zqda = questions[activeQuestion].questionAnswer;
+	if (zqda == $("#" + id).find("input[name=item]:checked").val()) {
+		$("#" + id).addClass("clickTrim");
+		for (var i = 0; i < checkQues.length; i++) {
+			if (checkQues[i].id == activeQuestion && checkQues[i].item != id) {
+				ques = checkQues[i].id;
+				checkQues[i].item = id;// 获取当前考题的选项ID
+				checkQues[i].answer = $("#" + id).find(
+						"input[name=item]:checked").val();// 获取当前考题的选项值
+				checkQues[i].zqda = 1;
+			}
+		}
+
+		if (checkQues.length == 0 || Question != activeQuestion
+				&& activeQuestion != ques) {
+			var check = {};
+			check.id = activeQuestion;// 获取当前考题的编号
+			check.item = id;// 获取当前考题的选项ID
+			check.answer = $("#" + id).find("input[name=item]:checked").val();// 获取当前考题的选项值
+			check.zqda = 1;
+			checkQues.push(check);
+		}
+		if ((activeQuestion + 1) != questions.length) {
+			showQuestion(activeQuestion + 1);
+		} else {
+			showQuestion(activeQuestion)
+		}
+	} else {
+		$("#" + id).addClass("clickTrimcw");
+		for (var i = 0; i < checkQues.length; i++) {
+			if (checkQues[i].id == activeQuestion && checkQues[i].item != id) {
+				ques = checkQues[i].id;
+				checkQues[i].item = id;// 获取当前考题的选项ID
+				checkQues[i].answer = $("#" + id).find(
+						"input[name=item]:checked").val();// 获取当前考题的选项值
+				checkQues[i].zqda = 0;
+			}
+		}
+
+		if (checkQues.length == 0 || Question != activeQuestion
+				&& activeQuestion != ques) {
+			var check = {};
+			check.id = activeQuestion;// 获取当前考题的编号
+			check.item = id;// 获取当前考题的选项ID
+			check.answer = $("#" + id).find("input[name=item]:checked").val();// 获取当前考题的选项值
+			check.zqda = 0;
+			checkQues.push(check);
+		}
+		// 把正确的答案显示出来
+		$(".question_info").each(function() {
+			var otherId = $(this).attr("id");
+			if (zqda == $("#" + otherId).find("input[name=item]").val()) {
+				zqid = otherId;
+				$("#" + otherId).addClass("clickTrim");
+			}
+		})
+		$('#jqhide').show();
+	}
+
+	$(".question_info").each(function() {
+		var otherId = $(this).attr("id");
+		if (otherId != id && otherId != zqid) {
+			$("#" + otherId).find("input").prop("checked", false);
+			$("#" + otherId).removeClass("clickTrim");
+			$("#" + otherId).removeClass("clickTrimcw");
+		}
+	})
+	Question = activeQuestion;
+	showctlv();
 
 }
 
-/*设置进度条*/
-function progress(){
-    var prog = ($(".active_question_id").length+1)/questions.length;
-    var pro = $(".progress").parent().width() * prog;
-    $(".progres").text((prog*100).toString().substr(0,5)+"%")
-    $(".progress").animate({
-        width: pro,
-        opacity: 0.5
-    }, 1000);
+/* 设置进度条 */
+function progress() {
+	var prog = ($(".active_question_id").length + 1) / questions.length;
+	var pro = $(".progress").parent().width() * prog;
+	$(".progres").text((prog * 100).toString().substr(0, 5) + "%")
+	$(".progress").animate({
+		width : pro,
+		opacity : 0.5
+	}, 1000);
 }
 
-/*保存考题状态 已做答的状态*/
-function saveQuestionState(clickId){
-    showQuestion(clickId)
+/* 保存考题状态 已做答的状态 */
+function saveQuestionState(clickId) {
+	showQuestion(clickId)
 }
 
-$(function(){
-    answerCard();
-    showQuestion(0);
-    showctlv();
-    /*alert(QuestionJosn.length);*/
-    /*实现进度条信息加载的动画*/
-    var str = '';
-    /*开启或者停止时间*/
-    $(".time-stop").click(function () {
-        timeState = false;
-        $(this).hide();
-        $(".time-start").show();
-    });
-    $(".time-start").click(function () {
-        timeState = true;
-        $(this).hide();
-        $(".time-stop").show();
-    });
+$(function() {
+	answerCard();
+	showQuestion(0);
+	showctlv();
 
-    /*收藏按钮的切换*/
-    $("#unHeart").click(function(){
-        $(this).hide();
-        $("#heart").show();
-    })
-    $("#heart").click(function(){
-        $(this).hide();
-        $("#unHeart").show();
-    })
+	/* alert(QuestionJosn.length); */
+	/* 实现进度条信息加载的动画 */
+	var str = '';
+	/* 开启或者停止时间 */
+	$(".time-stop").click(function() {
+		timeState = false;
+		$(this).hide();
+		$(".time-start").show();
+	});
+	$(".time-start").click(function() {
+		timeState = true;
+		$(this).hide();
+		$(".time-stop").show();
+	});
 
-    /*答题卡的切换*/
-    $("#openCard").click(function(){
-    	 $("#answerCardcw1").hide();
-        $("#answerCard").slideDown();
-       
-    })
-    
-  $("#openCardcw").click(function(){
-	  $("#answerCard").hide();
-	   answerCardcw();
-	   $("#answerCardcw1").show();
-    })
-    
-    $("#closeCard").click(function(){
-        $("#openCard").show();
-        $("#answerCard").slideUp();
-        $(this).hide();
-    })
+	/* 收藏按钮的切换 */
+	$("#unHeart").click(function() {
+		$(this).hide();
+		$("#heart").show();
+	})
+	$("#heart").click(function() {
+		$(this).hide();
+		$("#unHeart").show();
+	})
 
-    //提交试卷
-    $("#submitQuestions").click(function(){
-        /*alert(JSON.stringify(checkQues));*/
-        alert("已做答:"+($(".clickQue").length)+"道题,还有"+(questions.length-($(".clickQue").length))+"道题未完成");
-    })
-    //进入下一题
-    $("#nextQuestion").click(function(){
-        if((activeQuestion+1)!=questions.length){
-        	showQuestion(activeQuestion+1);
-        } else{
-        	 showQuestion(activeQuestion)
-        }
-    })
-    
-    $("#previousQuestion").click(function(){
-        if((activeQuestion-1)>=0){
-        	showQuestion(activeQuestion-1);
-        } else{
-        	 showQuestion(activeQuestion)
-        }
-    })
+	/* 答题卡的切换 */
+	$("#openCard").click(function() {
+		$("#answerCardcw1").hide();
+		$("#answerCard").slideDown();
+
+	})
+
+	$("#openCardcw").click(function() {
+		$("#answerCard").hide();
+		answerCardcw();
+		$("#answerCardcw1").show();
+	})
+
+	$("#closeCard").click(function() {
+		$("#openCard").show();
+		$("#answerCard").slideUp();
+		$(this).hide();
+	})
+
+	// 提交试卷
+	$("#submitQuestions").click(
+			function() {
+				var sum = [];
+				if (checkQues.length < 1) {
+					return;
+					} else {
+					for (var i = 0; i < checkQues.length; i++) {
+						if (checkQues[i].zqda == 1) {
+							sum.push(1);
+						}
+					}
+				}
+				$('#pjsp').removeClass('markedred')
+				$('#pjsp').removeClass('markedaqua')
+				$('#pjsp').removeClass('markedgreen')
+			if(sum.length<80 && 1==2){
+				$('#mlssimg').html('<img class="tjimg" src="../../static/jx/img/sy/mlss.jpeg"></img>')
+				$('#pjsp').html('超级马路杀手');
+				$('#pjsp').addClass('markedred')
+			}else if(sum.length<100 && sum.length>80){
+				$('#mlssimg').html('<img class="tjimg" src="../../static/jx/img/sy/nl.png"></img>')
+				$('#pjsp').html('继续加油');
+				$('#pjsp').addClass('markedaqua')
+			}else{
+				$('#mlssimg').html('<img  class="tjimg" src="../../static/jx/img/sy/100.jpeg"></img>')
+				$('#pjsp').html('秋名山车神');
+				$('#pjsp').addClass('markedgreen')
+			}
+			$('#pjspdesc').html("已做答:" +checkQues.length + "道题,错了"+eval(checkQues.length-sum.length)+"道题,还有"
+					+ (questions.length - checkQues.length)+ "道题未完成");
+				
+			})
+	// 进入下一题
+	$("#nextQuestion").click(function() {
+		if ((activeQuestion + 1) != questions.length) {
+			showQuestion(activeQuestion + 1);
+			$('#jqhide').hide();
+		} else {
+			showQuestion(activeQuestion)
+		}
+
+	})
+
+	$("#previousQuestion").click(function() {
+		if ((activeQuestion - 1) >= 0) {
+			showQuestion(activeQuestion - 1);
+		} else {
+			showQuestion(activeQuestion)
+		}
+	})
+
 })
-function sleep(numberMillis) { 
-	var now = new Date(); 
-	var exitTime = now.getTime() + numberMillis; 
-	while (true) { now = new Date(); 
-	if (now.getTime() > exitTime) return;   
-	} }
-/**算出错题率**/
-function showctlv(){
-	var sum=[];
-	if(checkQues.length<1){
+function sleep(numberMillis) {
+	var now = new Date();
+	var exitTime = now.getTime() + numberMillis;
+	while (true) {
+		now = new Date();
+		if (now.getTime() > exitTime)
+			return;
+	}
+}
+/** 算出错题率* */
+function showctlv() {
+	var sum = [];
+	if (checkQues.length < 1) {
 		$('.questionctl').text('(0/0)')
-	}else{
-		for(var i =0;i<checkQues.length;i++){
-			if(checkQues[i].zqda==0){
+	} else {
+		for (var i = 0; i < checkQues.length; i++) {
+			if (checkQues[i].zqda == 0) {
 				sum.push(1);
 			}
 		}
-		$('.questionctl').text('('+sum.length+'/'+checkQues.length+')')
+		$('.questionctl').text('(' + sum.length + '/' + checkQues.length + ')')
 	}
+}
+function musicplay() {
+	var t = $('.question_title').text()
+	var list = $('.question_info').text()
+	var url = "http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&text="
+			+ encodeURI(t);
+	var audio = new Audio(url);
+	audio.src = url;
+	audio.play();
+}
+function musicjqplay(){
+	var list = $('#showjq').text()
+	var url = "http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&text="
+			+ encodeURI(list);
+	var audio = new Audio(url);
+	audio.src = url;
+	audio.play();
 }
--- spring-boot-xcx/src/main/webapp/WEB-INF/jsp/xckmy/jqlxdt.jsp
+++ spring-boot-xcx/src/main/webapp/WEB-INF/jsp/xckmy/jqlxdt.jsp
@@ -27,11 +27,11 @@
 	src="${pageContext.request.contextPath}/static/jx/js/jquery-1.9.1.min.js"></script>
 <script
 	src="${pageContext.request.contextPath}/static/jx/js/bootstrap.js"></script>
-	<script
+<script
 	src="${pageContext.request.contextPath}/static/jx/js/Questions.js"></script>
 <style type="text/css">
 .warp {
-    width:100%;
+	width: 100%;
 	height: 90%;
 	position: relative;
 	overflow: auto;
@@ -44,112 +44,169 @@
 	<div class="warp">
 		<div class="col-md-10">
 			<div class="content">
-				
 
-				<div
-					style="width: 100%; height: auto; display: inline-block;">
+
+				<div style="width: 100%; height: auto; display: inline-block;">
 					<div style="width: 100%;">
 						<div style="width: 100%; margin: 0px auto">
-								<div class="middle-top"
-									style="width: 100%; height: 30px;  background: #2D3339; position: relative;">
-									<a href="">
-									<div class='top_left'><img alt="" width="30px" height="29px" src="${pageContext.request.contextPath}/static/jx/img/db/cz.png">重做</div></a>
-										<a id="submitQuestions" href="javascript:void(0)" ><div class='top_right'>提交<img alt="" width="30px" height="29px" src="${pageContext.request.contextPath}/static/jx/img/db/tj.png"></div></a>
-								</div>
-							
+							<div class="middle-top"
+								style="width: 100%; height: 30px; background: #2D3339; position: relative;">
+								<a href="">
+									<div class='top_left'>
+										<img alt="" width="30px" height="29px"
+											src="${pageContext.request.contextPath}/static/jx/img/db/cz.png">重做
+									</div>
+								</a> <a id="submitQuestions" href="javascript:void(0)"><div
+										class='top_right open-popup' data-target="#full">
+										提交<img alt="" width="30px" height="29px"
+											src="${pageContext.request.contextPath}/static/jx/img/db/tj.png">
+									</div></a>
+							</div>
+
 							<div
-								style="width: 100%; height: auto; display: inline-block; border-bottom: 1px dashed #CCC; background: #FFF;">
+								style="width: 100%; height: auto; display: inline-block; background: #FFF;">
 								<div
 									style="width: 100%; height: 90%; padding: 10px 20px 0px 20px;">
 									<!--试题区域-->
 									<ul class="list-unstyled question" id="" name="">
 										<li class="question_title"></li>
 									</ul>
-									
+
 								</div>
-		                      <span class=markedred>标红的字</span>
 							</div>
-							<!-- 答题卡 -->
+							<!-- 技巧 -->
+							<div id='jqhide' style="display: none;">
+							<div class="button_sp_area button_sp_area-zdy">
+							<ul style="list-style: none;"><li>
+							 <a onclick="musicjqplay()" href="javascript:;"  class=" weui-btn_default-zdy">
+        <img class='jq-left-img'  src="${pageContext.request.contextPath}/static/jx/img/sy/yy1.png">技巧:</a>
+        <span id='showjq'></span>
+							</li></ul>
+       
+      </div>
+							
+								<div class="personal-mybuluo-head">
+									<div class="personal-mybuluo-wording">试题详解</div>
+									<div class="personal-border jmu-border-1px border-bottom"></div>
+								</div>
+								<span class='bzda-db'></span>
+							</div>
+							<!-- 技巧 -->
 						</div>
 					</div>
 				</div>
-			
+
 				<!-- 底部 -->
-				
+
 			</div>
 		</div>
-		</div>
-		 <div class="weui-tabbar weui-tabbar-zdy">
-        <a id='previousQuestion' href="javascript:void(0)" class="weui-tabbar__item weui-bar__item--on">
-          <div class="weui-tabbar__icon">
-            <img src="${pageContext.request.contextPath}/static/jx/img/db/syt.png" alt="">
-          </div>
-          <p class="weui-tabbar__label">上一题</p>
-        </a>
-        <a id="unHeart" href="javascript:void(0)" class="weui-tabbar__item">
-          <div class="weui-tabbar__icon">
-           <img src="${pageContext.request.contextPath}/static/jx/img/db/sc.png" alt="">
-          </div>
-          <p class="weui-tabbar__label">收藏本题</p>
-        </a>
-        <a  id="heart" href="javascript:void(0)" class="weui-tabbar__item" style="color: #C40000; display: none;">
-          <div class="weui-tabbar__icon">
-           <img src="${pageContext.request.contextPath}/static/jx/img/db/bsc.png" alt="">
-          </div>
-          <p class="weui-tabbar__label">已收藏</p>
-        </a>
-        <a id='openCardcw' href="javascript:void(0)" class="weui-tabbar__item open-popup" data-target="#half">
-          <div class="weui-tabbar__icon">
-            <img src="${pageContext.request.contextPath}/static/jx/img/db/ct.png" alt="">
-          </div>
-          <p class="weui-tabbar__label">错题率<span class='questionctl'></span></p>
-        </a>
-       
-        <a id='openCard' href="javascript:void(0)" class="weui-tabbar__item open-popup" data-target="#half">
-           <div class="weui-tabbar__icon">
-            <img src="${pageContext.request.contextPath}/static/jx/img/db/sl.png" alt="">
-          </div>
-          <p class="weui-tabbar__label"><span class="questioned"></span>题/共<span
-												class="question_sum"></span>题</p>
-        </a>
-        <a id='nextQuestion' href="javascript:void(0)" class="weui-tabbar__item">
-          <div class="weui-tabbar__icon">
-            <img src="${pageContext.request.contextPath}/static/jx/img/db/xyt.png" alt="">
-          </div>
-          <p class="weui-tabbar__label">下一题</p>
-        </a>
-      </div>
+	</div>
+	<div class="weui-tabbar weui-tabbar-zdy">
+		<a id='previousQuestion' href="javascript:void(0)"
+			class="weui-tabbar__item weui-bar__item--on">
+			<div class="weui-tabbar__icon">
+				<img
+					src="${pageContext.request.contextPath}/static/jx/img/db/syt.png"
+					alt="">
+			</div>
+			<p class="weui-tabbar__label">上一题</p>
+		</a> <a id="unHeart" href="javascript:void(0)" class="weui-tabbar__item">
+			<div class="weui-tabbar__icon">
+				<img
+					src="${pageContext.request.contextPath}/static/jx/img/db/sc.png"
+					alt="">
+			</div>
+			<p class="weui-tabbar__label">收藏本题</p>
+		</a> <a id="heart" href="javascript:void(0)" class="weui-tabbar__item"
+			style="color: #C40000; display: none;">
+			<div class="weui-tabbar__icon">
+				<img
+					src="${pageContext.request.contextPath}/static/jx/img/db/bsc.png"
+					alt="">
+			</div>
+			<p class="weui-tabbar__label">已收藏</p>
+		</a> <a id='openCardcw' href="javascript:void(0)"
+			class="weui-tabbar__item open-popup" data-target="#half">
+			<div class="weui-tabbar__icon">
+				<img
+					src="${pageContext.request.contextPath}/static/jx/img/db/ct.png"
+					alt="">
+			</div>
+			<p class="weui-tabbar__label">
+				错题率<span class='questionctl'></span>
+			</p>
+		</a> <a id='openCard' href="javascript:void(0)"
+			class="weui-tabbar__item open-popup" data-target="#half">
+			<div class="weui-tabbar__icon">
+				<img
+					src="${pageContext.request.contextPath}/static/jx/img/db/sl.png"
+					alt="">
+			</div>
+			<p class="weui-tabbar__label">
+				<span class="questioned"></span>题/共<span class="question_sum"></span>题
+			</p>
+		</a> <a id='nextQuestion' href="javascript:void(0)"
+			class="weui-tabbar__item">
+			<div class="weui-tabbar__icon">
+				<img
+					src="${pageContext.request.contextPath}/static/jx/img/db/xyt.png"
+					alt="">
+			</div>
+			<p class="weui-tabbar__label">下一题</p>
+		</a>
+	</div>
 	<div id="half" class='weui-popup__container popup-bottom'>
+		<div class="weui-popup__overlay"></div>
+		<div class="weui-popup__modal">
+			<div class="toolbar">
+				<div class="toolbar-inner">
+					<a href="javascript:;" class="picker-button close-popup">关闭</a>
+
+				</div>
+			</div>
+			<div class="modal-content">
+				<!-- 展示答题卡 -->
+				<div id="answerCard" style="display: none;">
+					<div class="panel-body form-horizontal" style="padding: 0px;">
+						<ul class="list-unstyled">
+						</ul>
+					</div>
+				</div>
+				<!-- 展示错误的答题卡 -->
+				<div id="answerCardcw1" style="display: none;">
+					<div class="panel-body form-horizontal" style="padding: 0px;">
+						<ul class="list-unstyled list-unstyled-zdy">
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- 显示提交结果 -->
+	<div id="full" class='weui-popup__container'>
       <div class="weui-popup__overlay"></div>
       <div class="weui-popup__modal">
-        <div class="toolbar">
-          <div class="toolbar-inner">
-            <a href="javascript:;" class="picker-button close-popup">关闭</a>
-           
-          </div>
-        </div>
-        <div class="modal-content">
-        <!-- 展示答题卡 -->
-             <div id="answerCard" style="display: none;">
-											<div class="panel-body form-horizontal" style="padding: 0px;">
-												<ul class="list-unstyled">
-												</ul>
-											</div>
-										</div>
-	   <!-- 展示错误的答题卡 -->
-             <div id="answerCardcw1" style="display: none;">
-											<div class="panel-body form-horizontal" style="padding: 0px;">
-												<ul class="list-unstyled list-unstyled-zdy">
-												</ul>
-											</div>
-										</div>
-          </div>
-        </div>
+       <div class="warp">
+		<div class="weui-msg">
+			<div id='mlssimg' class="weui-msg__icon-area">
+				
+			</div>
+			<div class="weui-msg__text-area">
+				<h2 class="weui-msg__title" id='pjsp'></h2>
+				<p class="weui-msg__desc" id='pjspdesc'>
+				
+				</p>
+			</div>
+		</div>
+	</div>
+      
+        <a href="javascript:;" class="weui-btn weui-btn_primary close-popup">关闭</a>
       </div>
-<script
+    </div>
+	<script
 		src="${pageContext.request.contextPath}/static/all/weui/js/jquery-weui.js"></script>
 
-    
+
 
 </body>
 </html>
